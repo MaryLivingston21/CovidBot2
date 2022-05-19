37d8425dd8be373d35f02cf7b08b487a7f4d4aee
MaryLivingston21 <mliving1@uvm.edu>
2022-05-19 11:51:40 -0400

    - added covid risk for other offices - added option for weekly updates

--- main.py
+++ main.py
@@ -2,54 +2,79 @@ import logging
 import os
 import urllib.request, json
 
-from datetime import date
 from datetime import datetime
-from datetime import time
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
+os.environ['SLACK_BOT_TOKEN'] = ''
+
+
 # # WebClient instantiates a client that can call API methods
 # # When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+client = WebClient(token=os.environ.get('SLACK_BOT_TOKEN'))
 logger = logging.getLogger(__name__)
 # ID of the channel you want to send the message to
-channel_id = "U02AUH5TLHF"
+user_ids = {"U02AUH5TLHF": "Boston"}
+location_dict = {"Boston": ["Middlesex", "Massachusetts", "XXX"], "Durham": ["Durham", "North%20Carolina", "XXX"],
+                "Tampa": ["Pinellas", "Florida", "XXX"]}
 
 
-def get_message():
+def get_risk_level(location):
     with urllib.request.urlopen(
-            "https://data.cdc.gov/resource/3nnm-4jni.json?$limit=1&$order=date_updated%20DESC&county=Middlesex"
-            "%20County&state=Massachusetts") as url:
+            "https://data.cdc.gov/resource/3nnm-4jni.json?$limit=1&$order=date_updated%20DESC&county="
+            + location[1][0] + "%20County&state=" + location[1][1]) as url:
         data = json.loads(url.read().decode())
 
-    risk_level = data[0]['covid_19_community_level']
+    return data[0]['covid_19_community_level']
+
+
+def get_daily_message(location):
+    risk_level = get_risk_level(location)
     day_of_week = datetime.now().strftime("%A")
     month = datetime.now().strftime("%B")
     day = datetime.now().strftime("%d")
 
-    message = "Good Morning :wave: \nToday is " + \
-              day_of_week + " " + month + " " + day + ". \nThe covid risk level in Middlesex, MA is " + \
-              risk_level + " today. :mask: \n"
+    message = "Good Morning :wave: \nToday is " + day_of_week + " " + month + " " + day + \
+              ". \nThe covid risk level in " + location[0] + " is " + risk_level + \
+              ". :mask: \n"
 
     if day_of_week == ("Monday" or "Friday") or risk_level == "High":
-        message = message + "Working in-person is optional."
+        return message + "Working in-person is optional today.\n"
 
-    return message
+    return message + "Working in-person is required today.\n"
 
 
-if datetime.now().hour < 7:
-    today = date.today()
-    scheduled_time = time(hour=7)
-    schedule_timestamp = datetime.combine(today, scheduled_time).strftime('%s')
-    try:
-        result = client.chat_scheduleMessage(channel=channel_id, text=get_message(),
-                                             post_at=schedule_timestamp)
-        logger.info(result)
-    except SlackApiError as e:
-        logger.error("Error scheduling message: {}".format(e))
-else:
-    try:
-        result = client.chat_postMessage(channel=channel_id, text=get_message())
-        logger.info(result)
-    except SlackApiError as e:
-        logger.error("Error scheduling message: {}".format(e))
+def get_weekly_message(location):
+    risk_level = get_risk_level(location)
+
+    message = "Good Morning :wave: \nThe covid risk level in " + location[0] + \
+              " is " + risk_level + ". :mask: \n"
+
+    if risk_level == "High":
+        return message + "Working in-person is optional next week.\n"
+
+    return message + "Working in-person is required Tues, Wed, and Thurs, next week.\n"
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+
+    if datetime.now().strftime("%a") == "Fri":
+        for location in location_dict.items():
+            try:
+                result = client.chat_postMessage(channel=location[1][2],
+                                                 text=get_weekly_message(location))
+                logger.info(result)
+            except SlackApiError as e:
+                logger.error("Error posting message: {}".format(e))
+
+    for user in user_ids.items():
+        location = (user[1], location_dict.get(user[1]))
+        try:
+            result = client.chat_postMessage(channel=user[0],
+                                             text=get_daily_message(location))
+            logger.info(result)
+        except SlackApiError as e:
+            logger.error("Error posting message: {}".format(e))
